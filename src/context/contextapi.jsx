6abae14ddf6c2eca21a38de15dc25d2d033fa068
PRAJAPATI HARSH <harshprajapati8583@gmail.com>
2024-06-18 22:22:32 +0530

    aws

--- src/context/contextapi.jsx
+++ src/context/contextapi.jsx
@@ -7,9 +7,9 @@ export const Authcontext = createContext();
 export const MycontextProvider = ({ children }) => {
   // const url = import.meta.env.REACT_API;
   // const url = import.meta.env.VITE_REACT_API;
-  const url = import.meta.env.VITE_RENDER_API;
+  // const url = import.meta.env.VITE_RENDER_API;
+  const url = import.meta.env.VITE_AWS_API;
 
-  
   // const url = "http://localhost:3000";
   // const url = "http://15.207.255.221:5001"
   // const url = "https://kb-kc44.onrender.com";
