ab13419845bf0ea82cc270876a26ee87827817e0
PRAJAPATI HARSH <harshprajapati8583@gmail.com>
2024-07-26 22:00:21 +0530

    add something

--- src/context/contextapi.jsx
+++ src/context/contextapi.jsx
@@ -10,8 +10,8 @@ export const MycontextProvider = ({ children }) => {
   // const url = import.meta.env.VITE_RENDER_API;
   // const url = import.meta.env.VITE_AWS_API;
   
-  const url = "http://localhost:3000";
-  // const url = "https://backend-i3zq.onrender.com";
+  // const url = "http://localhost:3000";
+  const url = "https://backend-i3zq.onrender.com";
   // const url = "https://kb-kc44.onrender.com";
   const [token, settoken] = useState(localStorage.getItem("token"));
   const [user, setuser] = useState("");
